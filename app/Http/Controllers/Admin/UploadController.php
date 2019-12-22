aef649d5d01461d4d26eae8a0306224b09ede4eb
Murat <muratnysambaev93@gmail.com>
2019-12-22 13:14:15 +0600

    сохранение в storage public

--- app/Http/Controllers/Admin/UploadController.php
+++ app/Http/Controllers/Admin/UploadController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
@@ -14,7 +15,8 @@ class UploadController extends Controller
             'file' => 'required|image|mimes:jpg,jpeg,png',
         ]);
         $file = $request->file('file');
+        $path = $file->store('images', 'public');
 
-        return '/storage/' . $file->store('images', 'public');
+        return Storage::disk('public')->url($path);
     }
 }
